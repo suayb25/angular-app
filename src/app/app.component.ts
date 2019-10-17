86d5673f732d5ef1ab2579e60b07b8aeec1306b3
suayb25 <suayb.ozcelik@isik.edu.tr>
2019-10-17 19:44:15 +0300

    Model-Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'First App';
-  name = 'Şuayb Talha Özçelik';
+  name = 'Şuayb Talha';
+  items=[
+    {description:"Kahvaltı",action:"No"},
+    {description:"Sinema",action:"No"},
+    {description:"Spor",action:"No"},
+    {description:"Ders Çalışma",action:"No"},
+  ];
 }
