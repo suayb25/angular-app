1e6af7194fdb8075487ab90ebe6462c6c5c0524d
suayb25 <suayb.ozcelik@isik.edu.tr>
2019-10-17 19:07:17 +0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'First App';
+  name = 'Şuayb Talha Özçelik';
 }
