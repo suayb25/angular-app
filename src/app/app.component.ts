796bdb9633546141ad578a6b14441c28e5c6073d
suayb25 <suayb.ozcelik@isik.edu.tr>
2019-10-17 23:59:43 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model } from './model';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'First App';
-  name = 'Şuayb Talha Özçelik';
+ model=new Model();
+ getName(){ 
+  return this.model.user;
+ }
+ getItems(){
+   return this.model.items;
+ }
 }
