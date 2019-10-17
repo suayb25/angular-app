0bc60fad9efa505f01a633c97c3b1c5e3d39a0bc
suayb25 <suayb.ozcelik@isik.edu.tr>
2019-10-18 00:34:19 +0300

    Display All

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,19 @@ import { Model, Item } from './model';
 })
 export class AppComponent {
  model=new Model();
+ isDisplay=false;
  getName(){ 
   return this.model.user;
  }
  getItems(){
+   if(this.isDisplay){
+    return this.model.items;
+   }
    return this.model.items.filter(Item =>!Item.action) ;
+ }
+ addItem(value){
+   if(value!=""){
+    this.model.items.push(new Item(value,false));
+   }
  }
 }
