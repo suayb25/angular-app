e5fedbeaff7b81f47bfe80a70c726e1753a864c0
suayb25 <suayb.ozcelik@isik.edu.tr>
2019-10-29 16:48:20 +0300

    Order Record

--- src/app/shop/checkout/checkout.component.ts
+++ src/app/shop/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/model/order.model';
+import { OrderRepository } from 'src/app/model/order.repository';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'checkout',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  orderSent: boolean = false;
+  submitted:boolean = false;
+  constructor(public order:Order,private orderRepository:OrderRepository) { }
 
   ngOnInit() {
+  }
+
+  submitOrder(form: NgForm){
+    this.submitted = true;
+    if(form.valid){
+      this.orderRepository.saveOrder(this.order)
+      .subscribe(order=>{
+        this.order.clearOrder();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
   }
 
 }
